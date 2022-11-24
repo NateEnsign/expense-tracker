ad434cb925ae6b6fa3ebfb0aab4f061e25043c0e
Nate Ensign <dixienate@gmail.com>
2022-11-23 18:48:36 -0700

    project complete through day 3 instruction

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import Expenses from './components/Expenses';
+import React from 'react'
 
-function App() {
+import NewExpense from './components/NewExpense/NewExpense'
+import Expenses from './components/Expenses/Expenses';
+
+const App = () => {
   const expenses = [
     {
       id: "e1",
@@ -23,9 +26,15 @@ function App() {
     },
   ];
 
+  const addExpenseHandler = expense => {
+    console.log('In App.js')
+    console.log(expense)
+  }
+
+
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
